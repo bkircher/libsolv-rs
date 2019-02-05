43531adc614b9562b47a9062a8074af50e5bd800
Benjamin Kircher <benjamin.kircher@gmail.com>
2019-02-05 23:42:27 +0100

    whatrequires: no hard-coded path

--- examples/whatrequires.rs
+++ examples/whatrequires.rs
@@ -1,13 +1,17 @@
 use solv::{Pool, Repository};
-use std::path::Path;
 
 fn main() {
+    let path = std::env::args_os()
+        .skip(1)
+        .next()
+        .expect("usage: whatrequires <solv-file>");
+
     let mut pool = Pool::new();
     pool.set_arch("x86_64").expect("set repo arch");
 
     let mut updates = Repository::new(&mut pool, "updates").expect("create repository");
 
     updates
-        .set_solv_file(Path::new("/var/cache/dnf/updates.solv"), None)
+        .set_solv_file(path.as_ref(), None)
         .expect("set .solv file");
 }
